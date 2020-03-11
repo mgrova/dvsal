39d14cb167f305858975bc60766ade1587e65cb3
Marco Montes <marrcogrova@gmail.com>
2020-03-11 23:55:46 +0100

    added davis camera

---
+++ include/dvsal/streamers/CameraDavisStreamer.h
@@ -0,0 +1,51 @@
+//---------------------------------------------------------------------------------------------------------------------
+//  DVSAL
+//---------------------------------------------------------------------------------------------------------------------
+//  Copyright 2020 - Marco Montes Grova (a.k.a. mgrova) marrcogrova@gmail.com 
+//---------------------------------------------------------------------------------------------------------------------
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software 
+//  and associated documentation files (the "Software"), to deal in the Software without restriction, 
+//  including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+//  sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all copies or substantial 
+//  portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+//  BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+//  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES 
+//  OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+//  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//---------------------------------------------------------------------------------------------------------------------
+
+#ifndef CAMERA_DAVIS_STREAMER_H_
+#define CAMERA_DAVIS_STREAMER_H_
+
+#include <libcaercpp/devices/davis.hpp>
+
+#include <atomic>
+#include <csignal>
+
+#include <dvsal/streamers/Streamer.h>
+
+namespace dvsal{
+
+    class CameraDavisStreamer : public Streamer{
+    public:
+		bool init(const std::string &_string);
+		bool events(dv::EventStore &_events);
+        bool image(cv::Mat &_image); // Fake image using events
+        bool step();
+        bool cutUsingTime(int _microseconds);
+    private:
+        static void usbShutdownHandler(void *_ptr) ;
+    private:
+        libcaer::devices::davis *davisHandle_ = nullptr;        
+        constexpr static std::atomic<bool> globalShutdown_{false};
+
+        dv::EventStore events_;
+    };
+}
+
+#endif
