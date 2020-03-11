8040d8cedef04acfea7f9257bb14d1280f72a791
Marco Montes <marrcogrova@gmail.com>
2020-03-12 00:44:21 +0100

    minor change

--- src/processors/corner_detectors/utils/DistinctQueue.cpp
+++ src/processors/corner_detectors/utils/DistinctQueue.cpp
@@ -19,7 +19,7 @@
 //  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 //---------------------------------------------------------------------------------------------------------------------
 
-#include <dvsal/processors/corner_detectors/DistinctQueue.h>
+#include <dvsal/processors/corner_detectors/utils/DistinctQueue.h>
 
 namespace dvsal{
 
